f27afc5af95d86bc4897bce7d2af663f3e87c367
Weshy Joe <josephwaweru96@gmail.com>
2023-04-04 10:58:45 +0300

    models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,57 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'chat_users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sender')
+    recipient = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='recipient')
+    message = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        db_table = 'chat_messages'
+        verbose_name = 'Message'
+        verbose_name_plural = 'Message'
+        ordering = ('-date', )
+
+    # Get messages between two user
+    def get_all_messages(self, id_1, id_2):
+        messages = []
+
+        # Messages got sorted by date
+        # (sender to recipient)
+        message1 = Message.objects.filter(sender_id=id_1, recepient_id=id_2).order_by('-date')
+        for x in range(len(message1)):
+            messages.append(message1[x])
+
+        # Recipient to sender
+        message2 = Message.objects.filter(sender_id=id_2, receipient_id=id_1).order_by('-date')
+        for x in range(len(message2)):
+            messages.append(message2[x])
+
+        # Because the function is called when viewing the chat, we'll return all messages as read
+        for x in range(len(messages)):
+            messages[x].is_read = True
+
+        # Sort by date
+        messages.sort(key=lambda x: x.date, reverse=False)
+        return messages
+
--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -39,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+    'crispy_tailwind',
+
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +62,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(BASE_DIR / 'themes/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'theme/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
